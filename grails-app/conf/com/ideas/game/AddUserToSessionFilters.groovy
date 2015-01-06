1479a38f89ec6685a3074397dc851bcaccc864d7
Ashish Parkhi <ashishparkhi@yahoo.com>
2015-01-06 01:13:55 -0600

    getting star list only when the session is invalid.
    this is done to void network calls.

--- grails-app/conf/com/ideas/game/AddUserToSessionFilters.groovy
+++ grails-app/conf/com/ideas/game/AddUserToSessionFilters.groovy
@@ -16,37 +16,36 @@ class AddUserToSessionFilters {
         allURIs(uri: '/**'){
             before = {
                     println "User:"+request.getRemoteUser()+" IP:"+request.getRemoteAddr();
+                    BadgeService badgeService = new BadgeService(dataSource)
+                    TrophyService trophyService = new TrophyService(dataSource);
                     if(request.getRemoteUser() != null) {
+                        if(session.userInfo == null) {
 
-                        User user =  User.findByAccountName(stripDomainName(request.getRemoteUser()));
-                        if(user == null){
-                            UserInfoService userInfoService = new UserInfoService();
-                            UserInfoDTO dto = userInfoService.getUserInfoByUserName(request.getRemoteUser());
-                            if(dto == null) return;
+                            User user = User.findByAccountName(stripDomainName(request.getRemoteUser()));
+                            if (user == null) {
+                                UserInfoService userInfoService = new UserInfoService();
+                                UserInfoDTO dto = userInfoService.getUserInfoByUserName(request.getRemoteUser());
+                                if (dto == null) return;
 
-                            UserService userService = new UserService();
-                            user = userService.save(dto)
+                                UserService userService = new UserService();
+                                user = userService.save(dto)
+
+                            }
+
+                            session.userInfo = user;
+
+                            session.badges = badgeService.listAvailableBadges(session.userInfo.getDepartment().departmentName);
+                            session.starOfTheDayMap = trophyService.getStarOfTheDay(session.userInfo.getDepartment().departmentName);
+                            session.starOfTheWeekMap = trophyService.getStarOfTheWeek(session.userInfo.getDepartment().departmentName);
+                            session.starOfTheMonthMap = trophyService.getStarOfTheMonth(session.userInfo.getDepartment().departmentName);
+                            session.starMap = trophyService.getStar(session.userInfo.getDepartment().departmentName);
+                            session.appreciatorMap = trophyService.getAppreciator(session.userInfo.getDepartment().departmentName);
+                            MissionService missionService = new MissionService(dataSource);
+                            session.openMission = missionService.getOpenMissions();
+                            session.badgeLeaderBoard = badgeService.getBadgeLeaderBoard(session.userInfo.getDepartment().departmentName);
 
                         }
 
-                        session.userInfo=user;
-
-                        TrophyService trophyService = new TrophyService(dataSource);
-
-                        session.starOfTheDayMap = trophyService.getStarOfTheDay(session.userInfo.getDepartment().departmentName);
-                        session.starOfTheWeekMap = trophyService.getStarOfTheWeek(session.userInfo.getDepartment().departmentName);
-                        session.starOfTheMonthMap = trophyService.getStarOfTheMonth(session.userInfo.getDepartment().departmentName);
-                        session.starMap = trophyService.getStar(session.userInfo.getDepartment().departmentName);
-                        session.appreciatorMap = trophyService.getAppreciator(session.userInfo.getDepartment().departmentName);
-
-
-                        MissionService missionService = new MissionService(dataSource);
-                        session.openMission =
-                                missionService.getOpenMissions();
-
-                        BadgeService badgeService = new BadgeService(dataSource)
-                        session.badges = badgeService.listAvailableBadges(session.userInfo.getDepartment().departmentName);
-                        session.badgeLeaderBoard = badgeService.getBadgeLeaderBoard(session.userInfo.getDepartment().departmentName);
                 }
             }
         }
