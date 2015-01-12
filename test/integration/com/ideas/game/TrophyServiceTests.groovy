7376ed447629e94a9b4de091784a9c84c8403485
Ashish Parkhi <ashishparkhi@yahoo.com>
2015-01-12 06:03:47 -0600

    updated test

--- test/integration/com/ideas/game/TrophyServiceTests.groovy
+++ test/integration/com/ideas/game/TrophyServiceTests.groovy
@@ -1,54 +1,11 @@
 package com.ideas.game
 
-import com.ideas.game.Badge
-import com.ideas.game.Department
-import com.ideas.game.TrophyDTO
-import com.ideas.game.TrophyService
-import com.ideas.game.User
-import grails.test.mixin.*
 import org.junit.*
 
-/**
- * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
- */
-@TestFor(TrophyService)
+
+
 class TrophyServiceTests {
     def dataSource;
-
-    def static saveUser(String email, String firstName, String lastName, String departmentName, String employeeId){
-        User user = new User();
-        user.email=email;
-        user.department=Department.findByDepartmentName(departmentName);
-        user.firstName=firstName;
-        user.lastName=lastName;
-        user.employeeId=employeeId
-        user.accountName=employeeId
-        user.title="";
-        user.save(flush: true, failOnError: true);
-    }
-    private static void createBadge(String badgeName, boolean isDevil, String badgeIcon) {
-        Badge badge = new Badge();
-        badge.badgeName = badgeName;
-        badge.isEvil = isDevil;
-        badge.badgeIcon = badgeIcon;
-        badge.save(flush: true, failOnError: true);
-    }
-
-    private static void createDepatment(String departmentName){
-        Department department = new Department();
-        department.departmentName=departmentName;
-        department.save(flush: true, failOnError: true);
-    }
-    @BeforeClass
-    public static void setup(){
-        createDepatment("SD")
-        saveUser("test1.user1@ideas.com","test1","user1","SD","1");
-        saveUser("test2.user2@ideas.com","test2","user2","SD","2");
-
-    }
-
-
-
 
     @Test
     public void saveTrophies_negative_stars_should_not_affect_the_user_star_total(){
@@ -75,8 +32,7 @@ class TrophyServiceTests {
         Assert.assertEquals(User.findByEmail("test1.user1@ideas.com"),history.trophiesGivenBy)
     }
 
-    //TODO - to figureout why the second test does not run
-    //@Test
+    @Test
     public void saveTrophies_positive_stars_should_increment_user_star_total(){
 
         User user = User.findByEmail("test2.user2@ideas.com")
@@ -96,5 +52,33 @@ class TrophyServiceTests {
         user = User.findByEmail("test2.user2@ideas.com");
         Assert.assertEquals(12,user.trophies)
     }
+
+
+    def static saveUser(String email, String firstName, String lastName, String departmentName, String employeeId){
+        User user = new User();
+        user.email=email;
+        user.department=Department.findByDepartmentName(departmentName);
+        user.firstName=firstName;
+        user.lastName=lastName;
+        user.employeeId=employeeId
+        user.accountName=employeeId
+        user.title="";
+        user.save(flush: true, failOnError: true);
+    }
+
+    private static void createDepatment(String departmentName){
+        Department department = new Department();
+        department.departmentName=departmentName;
+        department.save(flush: true, failOnError: true);
+    }
+    @BeforeClass
+    public static void setup(){
+        createDepatment("SD")
+        saveUser("test1.user1@ideas.com","test1","user1","SD","1");
+        saveUser("test2.user2@ideas.com","test2","user2","SD","2");
+
+    }
+
+
 
 }
